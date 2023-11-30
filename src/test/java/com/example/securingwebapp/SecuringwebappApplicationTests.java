d629f3eb219bd0d5c99cd1692b87410a4a265daa
BruDevin <bruno.ads.2022@gmail.com>
2023-11-29 21:07:21 -0300

    Spring Security example

---
+++ src/test/java/com/example/securingwebapp/SecuringwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securingwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuringwebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
